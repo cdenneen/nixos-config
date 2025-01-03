c5a1e67ad538233dd6958b6c07070bfec9e7d158
Chris Denneen <cdenneen@gmail.com>
2025-01-03 14:38:30 -0500

    Try again

--- lib/mksystem.nix
+++ lib/mksystem.nix
@@ -24,6 +24,7 @@ let
   home-manager = if darwin then inputs.home-manager.darwinModules else inputs.home-manager.nixosModules;
 in systemFunc rec {
   inherit system;
+  inherit self;
 
   modules = [
     # Apply our overlays. Overlays are keyed by system type so we have
